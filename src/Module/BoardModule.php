46ee7dcd64a3b6a58b21c1d0c36d45c0456d9f62
KiVeR <bossaton.nicolas@gmail.com>
2017-03-05 20:43:06 +0100

    update(BoardModule.php) : ajout de BoardLevel, BoardMode, BoardExercise et BoardTraining

--- src/Module/BoardModule.php
+++ src/Module/BoardModule.php
@@ -3,7 +3,11 @@
 namespace GlobalExam\Api\Sdk\Module;
 
 use GlobalExam\Api\Sdk\Resource\Board\Board;
+use GlobalExam\Api\Sdk\Resource\Board\BoardExercise;
+use GlobalExam\Api\Sdk\Resource\Board\BoardLevel;
+use GlobalExam\Api\Sdk\Resource\Board\BoardMode;
 use GlobalExam\Api\Sdk\Resource\Board\BoardSection;
+use GlobalExam\Api\Sdk\Resource\Board\BoardTraining;
 
 trait BoardModule
 {
@@ -17,10 +21,42 @@ trait BoardModule
     }
 
     /**
+     * @return BoardLevel
+     */
+    public function boardLevel()
+    {
+        return new BoardLevel($this);
+    }
+
+    /**
+     * @return BoardMode
+     */
+    public function boardMode()
+    {
+        return new BoardMode($this);
+    }
+
+    /**
      * @return BoardSection
      */
     public function boardSection()
     {
         return new BoardSection($this);
+    }
+
+    /**
+     * @return BoardExercise
+     */
+    public function boardExercise()
+    {
+        return new BoardExercise($this);
+    }
+
+    /**
+     * @return BoardTraining
+     */
+    public function boardTraining()
+    {
+        return new BoardTraining($this);
     }
 }
