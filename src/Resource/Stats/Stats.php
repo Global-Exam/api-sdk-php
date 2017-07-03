d1eadabafde7297b8ae3f7ec02fb2ad851d3b725
Syl <sylvain.valienne@global-exam.com>
2017-07-03 10:15:47 +0200

    added stats module

---
+++ src/Resource/Stats/Stats.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace GlobalExam\Api\Sdk\Resource\Stats;
+
+use GlobalExam\Api\Sdk\Api;
+use GlobalExam\Api\Sdk\Resource\Resource;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class Stats
+ *
+ * @package GlobalExam\Api\Sdk\Resource\Stats
+ */
+class Stats
+{
+    use Resource;
+
+    const RESOURCE_KEY = 'stats';
+
+    /**
+     * Skill constructor.
+     *
+     * @param Api $api
+     */
+    public function __construct(Api $api)
+    {
+        $this->api = $api;
+    }
+
+    /**
+     * @param array $body
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function global(array $body = [])
+    {
+        return $this->api->send('POST', static::RESOURCE_KEY . '/global', $body);
+    }
+
+    /**
+     * @param array $body
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function trainingMode(array $body = [])
+    {
+        return $this->api->send('POST', static::RESOURCE_KEY . '/training-mode', $body);
+    }
+
+    /**
+     * @param array $body
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function examMode(array $body = [])
+    {
+        return $this->api->send('POST', static::RESOURCE_KEY . '/exam-mode', $body);
+    }
+}
