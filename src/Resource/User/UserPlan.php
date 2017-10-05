912e07c782d6236352df94679e4b78d68134ddc7
Syl <sylvain.valienne@global-exam.com>
2017-10-05 10:30:11 +0200

    added requests

--- src/Resource/User/UserPlan.php
+++ src/Resource/User/UserPlan.php
@@ -3,7 +3,10 @@
 namespace GlobalExam\Api\Sdk\Resource\User;
 
 use GlobalExam\Api\Sdk\Api;
+use GlobalExam\Api\Sdk\Resource\Board\Board;
+use GlobalExam\Api\Sdk\Resource\Coupon\Coupon;
 use GlobalExam\Api\Sdk\Resource\Resource;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Class UserPlan
@@ -24,5 +27,27 @@ class UserPlan
     public function __construct(Api $api)
     {
         $this->api = $api;
+    }
+
+    /**
+     * @param       $id
+     * @param array $params
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function getBoards($id, array $params = [])
+    {
+        return $this->api->send('GET', static::RESOURCE_KEY . '/' . $id . '/relationships/' . Board::RESOURCE_KEY, [], $params);
+    }
+
+    /**
+     * @param       $id
+     * @param array $params
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function getCoupons($id, array $params = [])
+    {
+        return $this->api->send('GET', static::RESOURCE_KEY . '/' . $id . '/relationships/' . Coupon::RESOURCE_KEY, [], $params);
     }
 }
