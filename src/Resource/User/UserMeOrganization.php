cb49704f6b4a2356246eaa3df7d1df2037a5412f
Syl <sylvain.valienne@global-exam.com>
2017-12-28 14:23:04 +0100

    added requests

--- src/Resource/User/UserMeOrganization.php
+++ src/Resource/User/UserMeOrganization.php
@@ -3,6 +3,8 @@
 namespace GlobalExam\Api\Sdk\Resource\User;
 
 use GlobalExam\Api\Sdk\Api;
+use GlobalExam\Api\Sdk\Resource\Organization\OrganizationGroup;
+use GlobalExam\Api\Sdk\Resource\Organization\OrganizationLicense;
 use GlobalExam\Api\Sdk\Resource\Resource;
 use Psr\Http\Message\ResponseInterface;
 
@@ -36,5 +38,132 @@ class UserMeOrganization
     public function search(array $body = [], array $params = [])
     {
         return $this->api->send('POST', self::RESOURCE_KEY . '/search', $body, $params);
+    }
+
+    /**
+     * @param       $id
+     * @param array $params
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function getUsers($id, array $params = [])
+    {
+        return $this->api->send('GET', self::RESOURCE_KEY . '/' . $id . '/relationships/' . User::RESOURCE_KEY, [], $params);
+    }
+
+    /**
+     * @param       $id
+     * @param       $userId
+     * @param array $params
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function getOneUser($id, $userId, array $params = [])
+    {
+        return $this->api->send('GET', self::RESOURCE_KEY . '/' . $id . '/relationships/' . User::RESOURCE_KEY . '/' . $userId, [], $params);
+    }
+
+    /**
+     * @param       $id
+     * @param array $params
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function getOrganizationGroups($id, array $params = [])
+    {
+        return $this->api->send('GET', self::RESOURCE_KEY . '/' . $id . '/' . OrganizationGroup::RESOURCE_KEY, [], $params);
+    }
+
+    /**
+     * @param       $id
+     * @param array $params
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function getOrganizationLicenses($id, array $params = [])
+    {
+        return $this->api->send('GET', self::RESOURCE_KEY . '/' . $id . '/' . OrganizationLicense::RESOURCE_KEY, [], $params);
+    }
+
+    /**
+     * @param       $id
+     * @param array $body
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function attachUser($id, array $body = [])
+    {
+        return $this->api->send('POST', static::RESOURCE_KEY . '/' . $id . '/relationships/' . User::RESOURCE_KEY, $body);
+    }
+
+    /**
+     * @param       $id
+     * @param array $body
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function detachUser($id, array $body = [])
+    {
+        return $this->api->send('DELETE', static::RESOURCE_KEY . '/' . $id . '/relationships/' . User::RESOURCE_KEY, $body);
+    }
+
+    /**
+     * @param       $id
+     * @param       $file
+     * @param array $data
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function basicUserImport($id, $file, array $data = [])
+    {
+        $body = [
+            [
+                'name'     => 'file',
+                'contents' => $file,
+            ],
+        ];
+
+        return $this->api->sendFile('POST', static::RESOURCE_KEY . '/' . $id . '/relationships/' . User::RESOURCE_KEY . '/basic-import', array_merge($body, $data));
+    }
+
+    /**
+     * @param       $id
+     * @param       $file
+     * @param array $data
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function advancedUserImport($id, $file, array $data = [])
+    {
+        $body = [
+            [
+                'name'     => 'file',
+                'contents' => $file,
+            ],
+        ];
+
+        return $this->api->sendFile('POST', static::RESOURCE_KEY . '/' . $id . '/relationships/' . User::RESOURCE_KEY . '/advanced-import', array_merge($body, $data));
+    }
+
+    /**
+     * @param       $id
+     * @param array $body
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function attachUserProvider($id, array $body = [])
+    {
+        return $this->api->send('POST', self::RESOURCE_KEY . '/' . $id . '/relationships/' . UserProvider::RESOURCE_KEY, $body);
+    }
+
+    /**
+     * @param       $id
+     * @param array $body
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function detachUserProvider($id, array $body = [])
+    {
+        return $this->api->send('DELETE', self::RESOURCE_KEY . '/' . $id . '/relationships/' . UserProvider::RESOURCE_KEY, $body);
     }
 }
