b88c2467edd0ccd40a9655fc7778429fcf6dd9f3
Syl <sylouuu@users.noreply.github.com>
2017-12-29 14:59:54 +0100

    Apply fixes from StyleCI (#18)
    [ci skip] [skip ci]

--- src/Module/OrganizationModule.php
+++ src/Module/OrganizationModule.php
@@ -7,8 +7,8 @@ use GlobalExam\Api\Sdk\Resource\Organization\OrganizationBusinessType;
 use GlobalExam\Api\Sdk\Resource\Organization\OrganizationGroup;
 use GlobalExam\Api\Sdk\Resource\Organization\OrganizationIp;
 use GlobalExam\Api\Sdk\Resource\Organization\OrganizationLicense;
-use GlobalExam\Api\Sdk\Resource\Organization\UserMeOrganizationStats;
 use GlobalExam\Api\Sdk\Resource\Organization\OrganizationType;
+use GlobalExam\Api\Sdk\Resource\Organization\UserMeOrganizationStats;
 
 /**
  * Class OrganizationModule
