5e47bd4f7e8b48b13958224c1defd704eac11473
Syl <sylvain.valienne@global-exam.com>
2018-02-16 17:56:00 +0100

    updated requests

--- src/Resource/User/UserMeOrganizationStats.php
+++ src/Resource/User/UserMeOrganizationStats.php
@@ -82,54 +82,6 @@ class UserMeOrganizationStats
      *
      * @return mixed|ResponseInterface
      */
-    public function getActivity(int $id, array $body = [], array $params = [])
-    {
-        return $this->api->send('POST', static::RESOURCE_KEY . '/' . $id . '/stats/activity', $body, $params);
-    }
-
-    /**
-     * @param int   $id
-     * @param array $body
-     * @param array $params
-     *
-     * @return mixed|ResponseInterface
-     */
-    public function getActivityExport(int $id, array $body = [], array $params = [])
-    {
-        return $this->api->send('POST', static::RESOURCE_KEY . '/' . $id . '/stats/activity/export', $body, $params);
-    }
-
-    /**
-     * @param int   $id
-     * @param array $body
-     * @param array $params
-     *
-     * @return mixed|ResponseInterface
-     */
-    public function getLogin(int $id, array $body = [], array $params = [])
-    {
-        return $this->api->send('POST', static::RESOURCE_KEY . '/' . $id . '/stats/login', $body, $params);
-    }
-
-    /**
-     * @param int   $id
-     * @param array $body
-     * @param array $params
-     *
-     * @return mixed|ResponseInterface
-     */
-    public function getLoginExport(int $id, array $body = [], array $params = [])
-    {
-        return $this->api->send('POST', static::RESOURCE_KEY . '/' . $id . '/stats/login/export', $body, $params);
-    }
-
-    /**
-     * @param int   $id
-     * @param array $body
-     * @param array $params
-     *
-     * @return mixed|ResponseInterface
-     */
     public function getTraining(int $id, array $body = [], array $params = [])
     {
         return $this->api->send('POST', static::RESOURCE_KEY . '/' . $id . '/stats/training', $body, $params);
@@ -193,6 +145,54 @@ class UserMeOrganizationStats
     public function getSkillExport(int $id, array $body = [], array $params = [])
     {
         return $this->api->send('POST', static::RESOURCE_KEY . '/' . $id . '/stats/skill/export', $body, $params);
+    }
+
+    /**
+     * @param int   $id
+     * @param array $body
+     * @param array $params
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function getActivity(int $id, array $body = [], array $params = [])
+    {
+        return $this->api->send('POST', static::RESOURCE_KEY . '/' . $id . '/stats/activity', $body, $params);
+    }
+
+    /**
+     * @param int   $id
+     * @param array $body
+     * @param array $params
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function getActivityExport(int $id, array $body = [], array $params = [])
+    {
+        return $this->api->send('POST', static::RESOURCE_KEY . '/' . $id . '/stats/activity/export', $body, $params);
+    }
+
+    /**
+     * @param int   $id
+     * @param array $body
+     * @param array $params
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function getLogin(int $id, array $body = [], array $params = [])
+    {
+        return $this->api->send('POST', static::RESOURCE_KEY . '/' . $id . '/stats/login', $body, $params);
+    }
+
+    /**
+     * @param int   $id
+     * @param array $body
+     * @param array $params
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function getLoginExport(int $id, array $body = [], array $params = [])
+    {
+        return $this->api->send('POST', static::RESOURCE_KEY . '/' . $id . '/stats/login/export', $body, $params);
     }
 
     /**
