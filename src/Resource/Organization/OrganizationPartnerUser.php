c9839d5ee2c2585098a486fd4849cc70d8382903
Syl <sylvain.valienne@global-exam.com>
2018-02-28 10:46:30 +0100

    added request

---
+++ src/Resource/Organization/OrganizationPartnerUser.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace GlobalExam\Api\Sdk\Resource\Organization;
+
+use GlobalExam\Api\Sdk\Api;
+use GlobalExam\Api\Sdk\Resource\Resource;
+use GlobalExam\Api\Sdk\Resource\User\User;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class OrganizationPartnerUser
+ *
+ * @package GlobalExam\Api\Sdk\Resource\Organization
+ */
+class OrganizationPartnerUser
+{
+    use Resource;
+
+    const RESOURCE_KEY = 'organization';
+
+    /**
+     * OrganizationType constructor.
+     *
+     * @param Api $api
+     */
+    public function __construct(Api $api)
+    {
+        $this->api = $api;
+    }
+
+    /**
+     * @param       $id
+     * @param array $body
+     *
+     * @return mixed|ResponseInterface
+     */
+    public function detachUser($id, array $body = [])
+    {
+        return $this->api->send('DELETE', static::RESOURCE_KEY . '/' . $id . '/partner/relationships/' . User::RESOURCE_KEY, $body);
+    }
+}
